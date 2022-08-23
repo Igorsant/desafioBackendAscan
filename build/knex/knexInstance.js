c77f62d26f69e9fa35d516902cc3b601da6ceccd
Igor Santos <gorsantos381@gmail.com>
2022-08-23 20:07:43 -0300

    updating setup, deleting build folder

--- build/knex/knexInstance.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.knexInstance = void 0;
-const knexfile_1 = __importDefault(require("../knexfile"));
-const knex_1 = require("knex");
-exports.knexInstance = (0, knex_1.knex)(knexfile_1.default[process.env.NODE_ENV || 'development']);
--- build/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UsuarioRoute_1 = require("./UsuarioRoute");
-const Resolver = (app) => {
-    (0, UsuarioRoute_1.UsuarioRoutes)(app);
-};
-exports.default = Resolver;
